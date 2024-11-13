cbdcfa84922d3d72aae7a26310bdf3fc7846865a
YoNKouu23 <emmanuelpornel1@gmail.com>
2024-11-13 19:31:52 +0800

    added the news/update and announcement files and its content

---
+++ webapp/sample/announcement.js
@@ -0,0 +1,44 @@
+export const announcement = [
+    {
+        id: 1,
+        title: '#WalangPasok',
+        description: 'Suspendido ang lahat ng face-to-face na klase sa lahat ng antas bukas, Nobyembre 11, sa buong lalawigan ng Rizal ayon kay Gobernadora Nina Ynares dahil sa lagay ng panahon. Patuloy na ipapatupad ang mga alternatibong paraan ng pag-aaral gaya ng online, modular, asynchronous, at iba pa. Mangyaring hintayin ang karagdagang detalye mula sa inyong mga propesor.',
+        date: '2024-11-11',
+        image: require("@/public/images/announcement/walang-pasok.jpg")
+    },
+    {
+        id: 1,
+        title: 'Long Weekend Advisory!',
+        description: "Heads up, Colegio de Montalban community! As per Memorandum Circular No. 67, s. 2024 of the Office of the President, there will be an earlier weekend break in observance of the following days: Thursday, Oct 31 – Half-day starting at 12:00 PM Friday, Nov 1 – All Saints' Day   Saturday, Nov 2 – All Souls' Day   Sunday, Nov 3 – Weekend   Take this time to rest, reflect, and recharge. Classes and office operations will resume as usual on Monday, November 4. Enjoy the long weekend, everyone!",
+        date: '2024-10-28',
+        image: require("@/public/images/announcement/long-weekend.jpg")
+    },
+    {
+        id: 1,
+        title: '#Walang Pasok',
+        description: 'Classes at all levels in Montalban will remain suspended tomorrow, October 25, according to the latest announcement from the Local Government. Colegio de Montalban is implementing alternative teaching methods during calamities. Professors are entrusted to monitor the weather and decide on the appropriate alternative mode of teaching.Additionally, unless a new announcement is made suspending government work tomorrow, professors and staff are expected to report for duty. However, they are given the discretion to decide whether it is safe to come to work based on the weather conditions. Safety is the top priority, CdMians!',
+        date: '2023-10-24',
+        image: require("@/public/images/announcement/walang-pasok.jpg")
+    },
+    {
+        id: 1,
+        title: 'Walang Pasok',
+        description: 'Mananatiling walang pasok sa lahat ng antas ng paaralan sa Bayan ng Montalban bukas ika-24 ng Oktubre ayon sa pinakahuling anunsyo ng Lokal na Pamahalaan. Pinapatupad naman ng Colegio de Montalban ang mga alternatibong paraan ng pagtuturo sa panahon ng kalamidad. Ipinapaubaya sa mga propesor ang pag-antabay sa lagay ng panahon para sa karapat-dapat na alternatibong paraan ng pagtuturo. Karagdagan, kung walang panibagong anunsyo sa pagsuspinde ng trabaho sa pamahalaan sa araw ng bukas ay kailangan mag-ulat ng mga propesor at kawani ng kolehiyo. Ipinapaubaya parin sa bawat propesor at kawani ang pagpasya sa pagpasok kung ang lagay ng panahon ay hindi pinahihintulutan. Kaligtasan ang pangunahing prayoridad CdMians!',
+        date: '2024-10-23',
+        image: require("@/public/images/announcement/class-suspension-update.jpg")
+    },
+    {
+        id: 1,
+        title: 'JUST IN:',
+        description: ' Following the President’s announcement to suspend government work and classes across all levels in Luzon, due to #KristinePH, all alternative modes of instruction—synchronous, asynchronous, and modular—are also suspended. Faculty are not required to report to the office, and administrative offices, including the Registrar, will remain closed. Please note that affected classes will have make-up sessions to ensure compliance with the required number of instructional hours as outlined in the course syllabus. Stay safe, CdMians!',
+        date: '2024-10-22',
+        image: require("@/public/images/announcement/class-suspension-update.jpg")
+    },
+    {
+        id: 1,
+        title: 'E-Library',
+        description: "Great news, CdMians! The E-Library is now open! Whether you're at home or on the go, reference materials are now just a click away. Access anytime, anywhere! Log in through the student portal and explore a world of resources at your fingertips. Let's make studying more convenient this semester!",
+        date: '2024-10-22',
+        image: require("@/public/images/announcement/e-library.jpg")
+    },
+]
---
+++ webapp/sample/news-update.js
@@ -0,0 +1,53 @@
+export const news = [
+    {
+        id: 1,
+        title: 'Congratulations to the 𝐂𝐃𝐌 𝐆𝐨𝐥𝐝𝐞𝐧 𝐄𝐚𝐠𝐥𝐞𝐬 and all 𝐂𝐨𝐥𝐞𝐠𝐢𝐨 𝐝𝐞 𝐌𝐨𝐧𝐭𝐚𝐥𝐛𝐚𝐧 𝐒𝐭𝐮𝐝𝐞𝐧𝐭𝐬 for claiming the prestigious titles of 𝐆𝐫𝐚𝐧𝐝 𝐂𝐡𝐚𝐦𝐩𝐢𝐨𝐧 and 𝐔𝐧𝐢𝐯𝐞𝐫𝐬𝐢𝐭𝐲 𝐕𝐨𝐭𝐢𝐧𝐠 𝐖𝐢𝐧𝐧𝐞𝐫 at the 𝐗𝐔𝐋 𝐔𝐧𝐢𝐯𝐞𝐫𝐬𝐢𝐭𝐲 𝐋𝐞𝐚𝐠𝐮𝐞 𝐒𝐞𝐚𝐬𝐨𝐧 𝟐 𝐇𝐎𝐊!',
+        description: 'These students’ hard work, dedication, and teamworkmade it all possible—from the intense 𝐇𝐎𝐊 battles and cosplay creativity to the university attendance, every photo, and each vote. Against all odds, they’ve shown that with passion and unity, no challenge is too great. We are proud of our students for proving that 𝐂𝐨𝐥𝐞𝐠𝐢𝐨 𝐝𝐞 𝐌𝐨𝐧𝐭𝐚𝐥𝐛𝐚𝐧 stands strong among the best. Well done, Golden Eagles!',
+        date: '2024-11-11',
+        image: require("@/public/images/news-update/cdm-news-1.jpg")
+    },
+    {
+        id: 1,
+        title: 'Tourism Sustainability Management and Cultural Arts!',
+        description: 'Ipinagmamalaki ng Colegio de Montalban ang matagumpay na seminar tungkol sa Tourism Sustainability Management and Cultural Arts na pinangunahan ng ating masipag na tagapagsalita, Dr. Jomelyn M. Abuan, at ginabayan ng ating NSTP Coordinator, Madam Nimfa Bautista. Lubos ang aming pasasalamat kay Mayor Gen. Ronnie S. Evangelista (Ret.) sa pagbibigay ng pagkakataong magkaroon ng makabuluhang talakayan at mga kapaki-pakinabang na paksa na tunay na nagbibigay inspirasyon sa aming mga mag-aaral. Maraming salamat sa inyong walang sawang suporta sa kaalaman at pagtataguyod ng kultura at turismo',
+        date: '2024-11-06',
+        image: require("@/public/images/news-update/cdm-news-2.jpg")
+    },
+    {
+        id: 1,
+        title: '𝗕𝗮𝗻𝗮𝗻𝗮 𝗘𝘅𝗵𝗶𝗯𝗶𝘁 𝟮𝟬𝟮𝟰',
+        description: "𝗢𝗻 𝗢𝗰𝘁𝗼𝗯𝗲𝗿 𝟯𝟬, 𝟮𝟬𝟮𝟰, 𝗧𝗵𝗶𝗿𝗱-𝘆𝗲𝗮𝗿 𝘀𝘁𝘂𝗱𝗲𝗻𝘁𝘀 𝗳𝗿𝗼𝗺 𝗕𝗦𝗘 𝗷𝗼𝗶𝗻𝗲𝗱 𝗮𝗻𝗱 𝘀𝗵𝗼𝘄𝗰𝗮𝘀𝗲𝗱 𝘁𝗵𝗲𝗶𝗿 𝗶𝗻𝗻𝗼𝘃𝗮𝘁𝗶𝘃𝗲 𝗯𝘂𝘀𝗶𝗻𝗲𝘀𝘀 𝗮𝗻𝗱 𝗲𝗻𝘁𝗿𝗲𝗽𝗿𝗲𝗻𝗲𝘂𝗿𝗶𝗮𝗹 𝗺𝗶𝗻𝗱𝘀𝗲𝘁𝘀 𝘁𝗵𝗿𝗼𝘂𝗴𝗵 𝘁𝗵𝗲 𝗯𝗮𝗻𝗮𝗻𝗮 𝗽𝗲𝗲𝗹 𝗲𝘅𝗵𝗶𝗯𝗶𝘁 𝗶𝗻 𝘁𝗵𝗲 𝗔𝗴𝗿𝗶𝗰𝘂𝗹𝘁𝘂𝗿𝗮𝗹 𝗕𝘂𝘀𝗶𝗻𝗲𝘀𝘀 𝗰𝗼𝘂𝗿𝘀𝗲, 𝘄𝗶𝘁𝗵 𝘁𝗵𝗲 𝘀𝘁𝘂𝗱𝗲𝗻𝘁 𝗵𝗮𝗻𝗱𝗹𝗲𝗱 𝗯𝘆 𝗣𝗿𝗼𝗳𝗲𝘀𝘀𝗼𝗿 𝗗𝗮𝗿𝗹𝗲𝗻𝗲 𝗖𝗿𝘂𝘇. 𝗢𝘂𝗿 𝘃𝗲𝗿𝘆 𝗼𝘄𝗻 𝗶𝗻𝗰𝗿𝗲𝗱𝗶𝗯𝗹𝗲 𝗜𝗕𝗘𝗦𝗖 𝗧𝗿𝗲𝗮𝘀𝘂𝗿𝗲𝗿, 𝗠𝘀. 𝗥𝗼𝗱𝗲𝘀𝘀𝗮 𝗗𝗲 𝗟𝗲𝗼𝗻, 𝘁𝗮𝗸𝗲𝘀 𝘁𝗵𝗲 𝗹𝗲𝗮𝗱 𝗼𝗻 𝘁𝗵𝗶𝘀 𝗲𝘃𝗲𝗻𝘁. 𝗜𝗻𝗱𝗲𝗲𝗱, 𝗶𝘁 𝘄𝗮𝘀 𝗮 𝘄𝗼𝗻𝗱𝗲𝗿𝗳𝘂𝗹 𝗲𝘃𝗲𝗻𝘁. 𝗧𝗵𝗲𝘆 𝘀𝗵𝗼𝘄𝗲𝗱 𝘁𝗵𝗲𝗶𝗿 𝘀𝗸𝗶𝗹𝗹𝘀 𝗮𝗻𝗱 𝗰𝗿𝗲𝗮𝘁𝗶𝘃𝗶𝘁𝘆 𝗮𝘀 𝗲𝗻𝘁𝗿𝗲𝗽𝗿𝗲𝗻𝗲𝘂𝗿𝘀' 𝘀𝘁𝘂𝗱𝗲𝗻𝘁𝘀 𝗮𝗻𝗱 𝘄𝗵𝗮𝘁 𝘁𝗵𝗲𝘆 𝗹𝗲𝗮𝗿𝗻𝗲𝗱 𝗶𝗻 𝘁𝗵𝗶𝘀 𝗰𝗼𝘂𝗿𝘀𝗲. 𝗧𝗵𝗶𝘀 𝗲𝘃𝗲𝗻𝘁 𝗮𝗹𝘀𝗼 𝗮𝗹𝗹𝗼𝘄𝗲𝗱 𝘁𝗵𝗲 𝘀𝘁𝘂𝗱𝗲𝗻𝘁𝘀 𝘁𝗼 𝗱𝗲𝘃𝗲𝗹𝗼𝗽 𝗮 𝘂𝗻𝗶𝗾𝘂𝗲 𝗽𝗿𝗼𝗱𝘂𝗰𝘁, 𝗱𝗲𝗺𝗼𝗻𝘀𝘁𝗿𝗮𝘁𝗶𝗻𝗴 𝘁𝗵𝗲𝗶𝗿 𝗱𝗲𝗱𝗶𝗰𝗮𝘁𝗶𝗼𝗻 𝘁𝗼 𝗯𝗲𝗰𝗼𝗺𝗶𝗻𝗴 𝘀𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹 𝗯𝘂𝘀𝗶𝗻𝗲𝘀𝘀 𝗹𝗲𝗮𝗱𝗲𝗿𝘀 𝗮𝗻𝗱 𝗲𝗻𝘁𝗿𝗲𝗽𝗿𝗲𝗻𝗲𝘂𝗿𝘀 𝗶𝗻 𝘁𝗵𝗲 𝗳𝘂𝘁𝘂𝗿𝗲.𝗖𝗼𝗻𝗴𝗿𝗮𝘁𝘂𝗹𝗮𝘁𝗶𝗼𝗻𝘀, 𝗕𝗦𝗘 𝘁𝗵𝗶𝗿𝗱-𝘆𝗲𝗮𝗿 𝘀𝘁𝘂𝗱𝗲𝗻𝘁𝘀! ",
+        date: '2024-10-30',
+        image: require("@/public/images/news-update/cdm-news-3.jpg")
+    },
+    {
+        id: 1,
+        title: 'Silid-Aralan Inc. at Colegio de Montalban!',
+        description: 'Masaya naming ibinabahagi ang tagumpay ng aming pinasimulang partnership, sa pangunguna ng ating Kagalang-galang na Punong Bayan at Tagapangulo ng Lupon ng mga Katiwala, Mayor Gen. Ronnie S. Evangelista (Ret.), at ni Sir Arcie G. Mallari, tagapagtatag ng Silid-Aralan Inc.  Tatlong daan at limampung (350) masisipag at masigasig na co-learners (facilitators) ang ngayon ay handang isabuhay ang aming (Silid-Aralan Inc.) misyon—ang pagyamanin at paghubog sa mga mag-aaral ng Montalban, Rizal! Lubos ang aming pasasalamat kay Ginoong Arcie G. Mallari para sa kanyang dedikasyon sa edukasyon at sa pakikipagtulungan upang isulong ang layunin ng Silid-Aralan sa ating komunidad. Gayundin, isang taos-pusong pasasalamat kay Mayor Evangelista para sa kanyang walang humpay na suporta at malasakit, na nagbigay-daan upang likhain ang isang maaalagang kapaligiran kung saan bawat mag-aaral ay may pagkakataong magtagumpay at maabot ang kanilang pinakamataas na potensyal. Sama-sama tayong magbubukas ng mas maraming oportunidad, magpapalaganap ng inspirasyon, at sisindihan ang pagmamahal sa pag-aaral sa ating komunidad. Halina’t samahan kami sa napakagandang paglalakbay na ito, habang sama-sama tayong nagtutulungan upang baguhin ang buhay ng ating mga mag-aaral!',
+        date: '2024-10-30',
+        image: require("@/public/images/news-update/cdm-news-4.jpg")
+    },
+    {
+        id: 1,
+        title: 'NSTP Seminars at Colegio de Montalban!',
+        description: 'Matagumpay ang mga NSTP seminars na isinagawa, kung saan tinalakay ang mahahalagang tema para sa mga estudyante. Noong Setyembre 25, 2024, isinagawa ang Anti-Drug Abuse Seminar kasama ang Municipal Anti-Drug Abuse Unit at Bureau of Jail Management and Penology. Sumunod naman noong Oktubre 9, 2024, sa pangunguna ni Ms. Angelique Mei A. Idlao-Agpao, Local Government Operations Officer, kung saan tinalakay ang Relevance of Participatory Governance to Good Governance and the Youth kasama ang panauhin mula sa Department of the Interior and Local Government. Maraming salamat sa lahat ng naging katuwang sa mga seminar na ito! At higit sa lahat, lubos kaming nagpapasalamat kay Gen. Ronnie S. Evangelista (Ret.), ang butihing Chairman ng Board of Trustees ng Colegio de Montalban at kasalukuyang Mayor ng Montalban, para sa kanyang matibay na suporta at adbokasiya para sa seryeng ito ng mga mahalagang NSTP seminars. Sama-sama tayong magtulungan para sa mas maliwanag na kinabukasan!',
+        date: '2022-01-01',
+        image: require("@/public/images/news-update/cdm-news-5-1.jpg"),
+        image: require("@/public/images/news-update/cdm-news-5-2.jpg")
+    },
+    {
+        id: 1,
+        title: '𝐄𝐍𝐆𝐋𝐈𝐒𝐇 𝐖𝐄𝐄𝐊 𝐂𝐄𝐋𝐄𝐁𝐑𝐀𝐓𝐈𝐎𝐍 ',
+        description: 'CDMians, brace yourselves into our 2-day event before the month ends! Introducing this year 𝐄𝐧𝐠𝐥𝐢𝐬𝐡 𝐖𝐞𝐞𝐤 𝐭𝐡𝐞𝐦𝐞𝐝 "𝐄𝐧𝐠𝐥𝐢𝐬𝐡 𝐚𝐬 𝐚 𝐆𝐥𝐨𝐛𝐚𝐥 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞: 𝐁𝐫𝐢𝐝𝐠𝐢𝐧𝐠 𝐂𝐮𝐥𝐭𝐮𝐫𝐞𝐬, 𝐂𝐨𝐧𝐧𝐞𝐜𝐭𝐢𝐧𝐠 𝐭𝐡𝐞 𝐖𝐨𝐫𝐥𝐝" this coming October 29 to October 30 with fun and exciting games and contests which you can be part of. • Scrabble - October 29 • Slogan Making Contest - October 29• Family Feud - October 29 • Cosplay Competition (any character from any english book or movie) - October 30 For registration, just fill up this link: https://docs.google.com/.../1PIhwFfrX0Y.../edit NO REGISTRATION FEE! Note: each participant are allowed to join maximum of 2 games/contest only.𝐉𝐮𝐬𝐭 𝐜𝐡𝐨𝐨𝐬𝐞 𝐰𝐡𝐞𝐫𝐞 𝐲𝐨𝐮 𝐰𝐚𝐧𝐧𝐚 𝐣𝐨𝐢𝐧 𝐚𝐧𝐝 𝐠𝐞𝐭 𝐚 𝐜𝐡𝐚𝐧𝐜𝐞 𝐭𝐨 𝐬𝐡𝐨𝐰𝐜𝐚𝐬𝐞 𝐲𝐨𝐮𝐫 𝐬𝐤𝐢𝐥𝐥𝐬 𝐚𝐧𝐝 𝐜𝐨𝐦𝐩𝐞𝐭𝐢𝐭𝐢𝐯𝐞𝐧𝐞𝐬𝐬. 𝐒𝐞𝐞 𝐲𝐨𝐮, 𝐂𝐃𝐌𝐢𝐚𝐧𝐬!',
+        date: '2024-10-20',
+        image: require("@/public/images/news-update/cdm-news-6.jpg")
+    }, 
+    {
+        id: 1,
+        title: 'Safe Passage: Navigating Challenges in the Great Outdoors',
+        description: 'Maraming salamat sa lahat ng dumalo sa matagumpay na seminar kasama ang ating resource speaker, Mr. Roderick C. Gonzales! Isang malaking pasasalamat din sa PE Department sa inisyatiba at sa pagsulong ng kaalaman tungkol sa ligtas at maayos na paglalakbay sa kalikasan.  Patuloy nating alamin ang mga tamang hakbang para sa isang ligtas at responsableng pag-explore ng great outdoors. Salamat sa inyong suporta!',
+        date: '2024-10-18',
+        image: require("@/public/images/news-update/cdm-news-7-1.jpg"),
+        image: require("@/public/images/news-update/cdm-news-7-2.jpg")
+    }
+]
