6f14e7ea2d05985d1706abe5d63fb09f300cb7dc
YoNKouu23 <emmanuelpornel1@gmail.com>
2024-11-16 15:02:34 +0800

    add the administration file

---
+++ webapp/sample/administration.js
@@ -0,0 +1,103 @@
+export const Administrations = [
+    {
+        name: "Dr. Joy U. Mercado, LPT",
+        role: "President, Colegio de Montalban",
+        image: require("@/public/images/sportclub/co-adviser-2.jpg")
+    },
+    {
+        name: "RHEZA MAUREEN Y. GABINETE, MBA, LPT",
+        role: "IOB DEAN",
+        image: require("@/public/images/sportclub/co-adviser-2.jpg")
+    },
+    {
+        name: "Artemio Paredes",
+        role: "ICS Adviser",
+        image: require("@/public/images/sportclub/co-adviser-2.jpg")
+    },
+    {
+        name: "Joel San Pascual",
+        role: "ICS Adviser",
+        image: require("@/public/images/sportclub/co-adviser-2.jpg")
+    },
+    {
+        name: "Lexter De Mesa",
+        role: "ICS Adviser",
+        image: require("@/public/images/sportclub/co-adviser-2.jpg")
+    },
+    {
+        name: "Rocendo Astillero",
+        role: "ICS Adviser",
+        image: require("@/public/images/sportclub/co-adviser-2.jpg")
+    },
+    {
+        name: "Emerson Facelo",
+        role: "ICS Adviser",
+        image: require("@/public/images/sportclub/co-adviser-2.jpg")
+    },
+    {
+        name: "Rosevelt Luat",
+        role: "ICS Adviser",
+        image: require("@/public/images/sportclub/co-adviser-2.jpg")
+    },
+    {
+        name: "Christian Cabero",
+        role: "ICS Adviser",
+        image: require("@/public/images/sportclub/co-adviser-2.jpg")
+    },
+    {
+        name: "John Julius Baldia",
+        role: "ICS Adviser",
+        image: require("@/public/images/sportclub/co-adviser-2.jpg")
+    },
+    {
+        name: "Medilyn Almazan",
+        role: "ICS Adviser",
+        image: require("@/public/images/sportclub/co-adviser-2.jpg")
+    },
+    {
+        name: "Elsa Calvez",
+        role: "ICS Adviser",
+        image: require("@/public/images/sportclub/co-adviser-2.jpg")
+    },
+    {
+        name: "Rhona Segui",
+        role: "ICS Adviser",
+        image: require("@/public/images/sportclub/co-adviser-2.jpg")
+    },
+    {
+        name: "Gretchen Anore ",
+        role: "ICS Adviser",
+        image: require("@/public/images/sportclub/co-adviser-2.jpg")
+    },
+    {
+        name: "Anndrea Fatimah Fermin ",
+        role: "ICS Adviser",
+        image: require("@/public/images/sportclub/co-adviser-2.jpg")
+    },
+    {
+        name: "Rizza Mae Brao",
+        role: "ICS Adviser",
+        image: require("@/public/images/sportclub/co-adviser-2.jpg")
+    },
+    {
+        name: "Jerwin Bahum",
+        role: "ICS Adviser",
+        image: require("@/public/images/sportclub/co-adviser-2.jpg")
+    },
+    {
+        name: "Michael Mades",
+        role: "ICS Adviser",
+        image: require("@/public/images/sportclub/co-adviser-2.jpg")
+    },
+    {
+        name: "Jonas Macapagal",
+        role: "ICS Adviser",
+        image: require("@/public/images/sportclub/co-adviser-2.jpg")
+    },
+    {
+        name: "Cherilyn Lamadora",
+        role: "ICS Adviser",
+        image: require("@/public/images/sportclub/co-adviser-2.jpg")
+    },
+
+]
